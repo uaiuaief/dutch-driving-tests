2d2acd0f6780fef0c5697b2bc38ddb9c6d0645eb
Jonathan <john@DESKTOP-HCRVJU7.localdomain>
2022-02-01 10:01:46 -0300

    update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,9 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'dutch_driving_tests.settings'
 import django
 django.setup()
 from django.db import IntegrityError
+from django.db.models.deletion import ProtectedError
 from test_centers import TEST_CENTERS
+from proxy_list import proxies
 from api import models
 from dotenv import load_dotenv
 
@@ -33,10 +35,22 @@ if __name__ == "__main__":
 
     # Add Test centers
     logger.info("Creating test centers")
-    models.TestCenter.objects.all().delete()
-    for tc in TEST_CENTERS:
-        instance = models.TestCenter(name=tc)
+
+    try:
+        models.TestCenter.objects.all().delete()
+        for tc in TEST_CENTERS:
+            instance = models.TestCenter(name=tc)
+            instance.save()
+    except ProtectedError as e:
+        logger.error("Couldn't delete test center instances", exc_info=True)
+
+    # Add Proxies
+    logger.info("Creating proxies")
+    models.Proxy.objects.all().delete()
+    for pr in proxies:
+        instance = models.Proxy(ip=pr)
         instance.save()
+
 
 
 
